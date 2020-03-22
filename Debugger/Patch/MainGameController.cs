f3d97577b15bf04c8d0e9c4d660d14d0592cb06c
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-22 22:58:40 +0000

    Merge resolve conflicts

---
+++ Debugger/Patch/MainGameController.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace Straitjacket.Subnautica.Mods.Debugger.Patch
+{
+    [HarmonyPatch(typeof(MainGameController))]
+    [HarmonyPatch("Update")]
+    static class MainGameController_Update
+    {
+        static void Postfix(MainGameController __instance)
+        {
+            if (Input.GetKeyUp(Debugger.Options.ToggleDebuggerVisibilityKey))
+            {
+                Debugger.Enabled = !Debugger.Enabled;
+            }
+        }
+    }
+}
