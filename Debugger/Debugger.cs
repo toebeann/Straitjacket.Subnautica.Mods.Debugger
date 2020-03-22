e10b75b17915db1343c4ca5818e48836e1419252
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-22 21:23:31 +0000

    Config options set up

---
+++ Debugger/Debugger.cs
@@ -0,0 +1,14 @@
+﻿using SMLHelper.V2.Handlers;
+
+namespace Straitjacket.Subnautica.Mods.Debugger
+{
+    internal class Debugger
+    {
+
+        public static Options Options = new Options();
+        public static void Initialise()
+        {
+            OptionsPanelHandler.RegisterModOptions(Options);
+        }
+    }
+}
