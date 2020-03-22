9bb78f05de5a266cbdab4bc72f9a285994c7cb62
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-22 21:23:31 +0000

    Config options set up

---
+++ Debugger/HarmonyPatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace Straitjacket.Subnautica.Mods.Debugger
+{
+    internal class HarmonyPatcher
+    {
+        public static void ApplyPatches()
+        {
+            var harmony = HarmonyInstance.Create("com.tobeyblaber.straitjacket.subnautica.debugger.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Debugger.Initialise();
+        }
+    }
+}
---
+++ Debugger/Options.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using LitJson;
+using SMLHelper.V2.Options;
+using UnityEngine;
+
+namespace Straitjacket.Subnautica.Mods.Debugger
+{
+    internal struct OptionsObject
+    {
+        public KeyCode ToggleDebuggerVisibilityKey { get; set; }
+    }
+
+    internal class Options : ModOptions
+    {
+        public KeyCode ToggleDebuggerVisibilityKey = KeyCode.F2;
+        private string ConfigPath
+            => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+
+        public Options() : base("Debugger")
+        {
+            KeybindChanged += Options_KeybindChanged;
+
+            if (!File.Exists(ConfigPath))
+            {
+                UpdateJSON();
+            }
+            else
+            {
+                ReadOptionsFromJSON();
+            }
+        }
+
+        public override void BuildModOptions()
+        {
+            AddKeybindOption("debuggerKey", "Show/hide Debugger",
+                GameInput.GetPrimaryDevice(), ToggleDebuggerVisibilityKey);
+        }
+
+        private void Options_KeybindChanged(object sender, KeybindChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void UpdateJSON()
+        {
+            var options = new OptionsObject
+            {
+                ToggleDebuggerVisibilityKey = ToggleDebuggerVisibilityKey
+            };
+
+            var stringBuilder = new StringBuilder();
+            JsonMapper.ToJson(options, new JsonWriter(stringBuilder)
+            {
+                PrettyPrint = true
+            });
+
+            File.WriteAllText(ConfigPath, stringBuilder.ToString());
+        }
+
+        private void ReadOptionsFromJSON()
+        {
+            if (File.Exists(ConfigPath))
+            {
+                string optionsJSON = File.ReadAllText(ConfigPath);
+                var options = JsonMapper.ToObject<OptionsObject>(optionsJSON);
+                var data = JsonMapper.ToObject(optionsJSON);
+                ToggleDebuggerVisibilityKey = data.ContainsKey("ToggleDebuggerVisibilityKey")
+                    ? options.ToggleDebuggerVisibilityKey : ToggleDebuggerVisibilityKey;
+                if (!data.ContainsKey("ToggleDebuggerVisibilityKey"))
+                {
+                    UpdateJSON();
+                }
+            }
+            else
+            {
+                UpdateJSON();
+            }
+        }
+    }
+}
